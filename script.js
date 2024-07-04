dab4038fcde7d227b4cf4cf28ccb2836bfcafee5
Nicolau <149939145+NicolauNavarro@users.noreply.github.com>
2024-07-04 12:55:22 +0200

    Portflio fail

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-/*const body = document.querySelector("body");
 
-window.addEventListener("load", () => {
-  body.classList.add("bodyShown");
-});*/
+
+
+const options = document.querySelectorAll('.options');
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        menutoggle(option)
+    });
+});
+function menutoggle(option){
+    let id = option.id
+        let menu = option.children[1].classList
+        let col = option.parentElement.parentElement.classList
+        if(id == 'false'){
+            option.classList.add('selectedbtn')
+            menu.add("expandMenu")
+            col.add('moveleft')
+            option.id = 'true'
+        }else{
+            option.classList.remove('selectedbtn')
+            menu.remove("expandMenu")
+            col.remove('moveleft')
+            option.id = 'false'
+        }
+}
+
+
+
+
+
