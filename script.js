2cb3cfb0f0dd64cf13eb2771b5632429ec91d5ef
Nicolau <149939145+NicolauNavarro@users.noreply.github.com>
2024-07-04 15:19:42 +0200

    GymBroApp main page

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
 
+const body = document.querySelector('body')
+window.addEventListener('load', () => {
+    body.style.display = 'flex';
+})
 
-
-const options = document.querySelectorAll('.options');
-options.forEach(option => {
-    option.addEventListener('click', () => {
-        menutoggle(option)
-    });
-});
-function menutoggle(option){
-    let id = option.id
-        let menu = option.children[1].classList
-        let col = option.parentElement.parentElement.classList
-        if(id == 'false'){
-            option.classList.add('selectedbtn')
-            menu.add("expandMenu")
-            col.add('moveleft')
-            option.id = 'true'
-        }else{
-            option.classList.remove('selectedbtn')
-            menu.remove("expandMenu")
-            col.remove('moveleft')
-            option.id = 'false'
-        }
-}
-
-
+document.querySelector('video').playbackRate = 0.8;
 
 
 
